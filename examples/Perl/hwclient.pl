c96cb242872b250eba80d056ed8b25e5dc844897
Chris Prather <chris@prather.org>
2010-12-04 18:57:49 -0500

    port hwclient and hwserver from Python

--- examples/Perl/hwclient.pl
+++ examples/Perl/hwclient.pl
@@ -1,13 +1,21 @@
-No-one has translated the hwclient example into Perl yet.  Be the first to create
-hwclient in Perl and get one free Internet!  If you're the author of the Perl
-binding, this is a great way to get people to use 0MQ in Perl.
+#!/usr/bin/env perl
+use 5.12.2;    # not neccessary but enables strict and modern features
 
-To submit a new translation email it to zeromq-dev@lists.zeromq.org.  Please:
+#   Hello World client in Perl (based on Python example)
+#   Connects REQ socket to tcp://localhost:5555
+#   Sends "Hello" to server, expects "World" back
 
-* Stick to identical functionality and naming used in examples so that readers
-  can easily compare languages.
-* You MUST place your name as author in the examples so readers can contact you.
-* You MUST state in the email that you license your code under the MIT/X11
-  license.
+use ZeroMQ qw/:all/;
 
-Subscribe to this list at http://lists.zeromq.org/mailman/listinfo/zeromq-dev.
+my $ctx    = ZeroMQ::Context->new;
+my $socket = $ctx->socket(ZMQ_REQ);
+$socket->connect('tcp://localhost:5555');
+
+for my $request ( 1 .. 10 ) {
+    say "Sending request $request";
+    $socket->send('Hello');
+
+    # Get the reply
+    my $message = $socket->recv();
+    say "Received reply ${ \$message->data } message";
+}
